41ea5a008d30c3694d1d599d35038e3533cedfb2
Duality224 <98560612+Duality224@users.noreply.github.com>
2023-02-27 19:37:50 +1300

    Update helpme.sh

--- helpme.sh
+++ helpme.sh
@@ -2,4 +2,4 @@
 
 #searching $* (all original arguments concatenated into a single string) in help, tldr, man and xdg default browser sequentially
 #stderr is routed to /dev/null (suppressing error messages)
-help $* || tldr $* || man $* || xdg-open "http://www.google.com/search?q=$*" 2>/dev/null
+help "$*" || tldr "$*" || man "$*" || xdg-open "http://www.google.com/search?q=$*" 2>/dev/null
